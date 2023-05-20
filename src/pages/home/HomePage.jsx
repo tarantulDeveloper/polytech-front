52622928b4e3efde548cbf217e4ce7ae2d143472
Bekzhan Satiev <tarantuldeveloper@gmail.com>
2023-05-20 20:22:14 +0600

    align news center

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,18 +1,25 @@
-import React from 'react';
+import React from "react";
 import Header from "../../components/header/Header";
 
 const HomePage = () => {
   return (
     <>
-      <Header/>
+      <Header />
       <div className="container">
         <div className="row">
-          <div className="col-sm-12 col-md-6">
+          <div className="col-sm-12 col-md-6 d-flex justify-content-center align-items-center">
+            <div>
             <h2>Новости о жизни ЛОВЗ</h2>
             <p>Объявления, связанные с жизнью ЛОВЗ</p>
+            </div>
+            
           </div>
-          <div className="col-sm-12 col-md-6">
-            <img className="img-fluid" src="https://kloop.kg/wp-content/uploads/2018/03/terminology_coalition-3.png" alt="Фото ЛОВЗ"/>
+          <div className="col-sm-12 col-md-6 d-flex justify-content-center align-items-center">
+            <img
+              className="img-fluid"
+              src="https://kloop.kg/wp-content/uploads/2018/03/terminology_coalition-3.png"
+              alt="Фото ЛОВЗ"
+            />
           </div>
         </div>
       </div>
