217f0b7eaa4196d7fb42e4d57bf8a1c695645e03
Bekzhan Satiev <tarantuldeveloper@gmail.com>
2023-05-21 17:49:46 +0600

    finish about us

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -6,12 +6,12 @@ import AboutUs from "../../components/about-us/AboutUs";
 const HomePage = () => {
   return (
     <>
-      <Header/>
+      <Header />
       <div className="container">
-        <Slider/>
-          <AboutUs />
+        <Slider />
+        <AboutUs />
         <div className="row mt-4">
-          <div className="col-sm-12 col-md-6 d-flex justify-content-center align-items-center">
+          <div className="col-sm-12 col-md-6 order-md-2 d-flex justify-content-center align-items-center">
             <div>
               <h2>Новости о жизни ЛОВЗ</h2>
               <p>Объявления, связанные с жизнью ЛОВЗ</p>
@@ -19,7 +19,7 @@ const HomePage = () => {
           </div>
           <div className="col-sm-12 col-md-6 d-flex  justify-content-center align-items-center">
             <img
-              className="img-fluid"
+              className="img-fluid "
               src="https://kloop.kg/wp-content/uploads/2018/03/terminology_coalition-3.png"
               alt="Фото ЛОВЗ"
             />
