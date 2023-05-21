b9142363c389534829ecd75f433f0c85105e2c21
Bekzhan Satiev <tarantuldeveloper@gmail.com>
2023-05-21 18:43:27 +0600

    add pointer class

--- src/components/news/News.jsx
+++ src/components/news/News.jsx
@@ -4,9 +4,9 @@ const News = () => {
   return (
         <div className="col-sm-12 col-md-4 col-lg-3">
             <div className="d-flex flex-column text-center">
-                <img src="https://i.pinimg.com/736x/41/f1/86/41f1865ffaba642d7feba2243c215817.jpg" alt="news image" className='img-fluid rounded'/>
-                <h4 className='my-1'>Заголовок новости</h4>
-                <p>Здесь будет подзаголовок, а подробнее о самой новости можно будет посмотреть
+                <img src="https://i.pinimg.com/736x/41/f1/86/41f1865ffaba642d7feba2243c215817.jpg" alt="news image" className='img-fluid rounded pointer'/>
+                <h4 className='my-1 pointer'>Заголовок новости</h4>
+                <p >Здесь будет подзаголовок, а подробнее о самой новости можно будет посмотреть
                     кликнув по ней
                 </p>
             </div>
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import '../node_modules/bootstrap/dist/js/bootstrap';
+import './Global.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
